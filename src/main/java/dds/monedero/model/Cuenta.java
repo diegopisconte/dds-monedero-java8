e6c712548b88ceddb92666b8a4c8c61b5821e67c
diegopisconte <diegopisconte@gmail.com>
2021-05-13 00:55:18 -0300

    Duplicated code - CUENTA (Ex) Linea 54

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -30,7 +30,7 @@ public class Cuenta {
     if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {
       throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
     }
-    
+
     this.agregarMovimiento(LocalDate.now(), cuanto, true);
   }
 
@@ -47,7 +47,7 @@ public class Cuenta {
       throw new MaximoExtraccionDiarioException("No puede extraer mas de $ " + 1000
           + " diarios, límite: " + limite);
     }
-    new Movimiento(LocalDate.now(), cuanto, false).agregateA(this);
+    this.agregarMovimiento(LocalDate.now(), cuanto, false);
   }
 
   public void agregarMovimiento(LocalDate fecha, double cuanto, boolean esDeposito) {
